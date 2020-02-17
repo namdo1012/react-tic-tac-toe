97f01faa904b75e3f429a8dedb3364f06ea0a169
namdo1012 <namdo1012199@gmail.com>
2020-02-17 11:31:02 +0700

    Updates

---
+++ precache-manifest.f273dff1a493552c1366756361b16a57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d2bc3a599f92cc7a01a8dc8f6ff6f1a",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "af9e1ebb3e6414821257",
+    "url": "/react-tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "d8b6ffee929070c962c8",
+    "url": "/react-tic-tac-toe/static/js/2.f473dba6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tic-tac-toe/static/js/2.f473dba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af9e1ebb3e6414821257",
+    "url": "/react-tic-tac-toe/static/js/main.32091b06.chunk.js"
+  },
+  {
+    "revision": "f67fddcf588b95e3209d",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.63526b18.js"
+  }
+]);
